b89a94bd94b553f27f8f0c970e308b847bfab03f
Yorso <jorgeguzon@gmail.com>
2017-03-04 07:53:33 +0100

    Changes

--- microspring9-cloud-client-lab8/src/main/java/com/jorge/LuckyWordController.java
+++ microspring9-cloud-client-lab8/src/main/java/com/jorge/LuckyWordController.java
@@ -1,6 +1,5 @@
 package com.jorge;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
